b4c7f9f54886580f559930c90d612e9a406d6a7c
YyzHarry <1400012996@pku.edu.cn>
2019-09-15 01:09:01 -0400

    add dqn related models

---
+++ sv_rl/dqn_model.py
@@ -0,0 +1,71 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DQN(nn.Module):
+
+    def __init__(self, in_channels=4, num_actions=18):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.fc4 = nn.Linear(7 * 7 * 64, 512)
+        self.fc5 = nn.Linear(512, num_actions)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.fc4(x.view(x.size(0), -1)))
+        return self.fc5(x)
+
+
+class DQN_RAM(nn.Module):
+
+    def __init__(self, in_features=4, num_actions=18):
+        super(DQN_RAM, self).__init__()
+        self.fc1 = nn.Linear(in_features, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, num_actions)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return self.fc4(x)
+
+
+class Dueling_DQN(nn.Module):
+
+    def __init__(self, in_channels=4, num_actions=18):
+        super(Dueling_DQN, self).__init__()
+        self.num_actions = num_actions
+
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+
+        self.fc1_adv = nn.Linear(in_features=7 * 7 * 64, out_features=512)
+        self.fc1_val = nn.Linear(in_features=7 * 7 * 64, out_features=512)
+
+        self.fc2_adv = nn.Linear(in_features=512, out_features=num_actions)
+        self.fc2_val = nn.Linear(in_features=512, out_features=1)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = x.view(x.size(0), -1)
+
+        adv = self.relu(self.fc1_adv(x))
+        val = self.relu(self.fc1_val(x))
+
+        adv = self.fc2_adv(adv)
+        val = self.fc2_val(val).expand(x.size(0), self.num_actions)
+
+        x = val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.num_actions)
+        return x
